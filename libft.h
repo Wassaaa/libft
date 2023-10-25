6806f45e859b817bc58c9205e57c18df2d79edfa
Allar Klein <aklein@c2r5p5.hive.fi>
2023-10-25 12:53:18 +0300

    deleted some more files

--- libft.h
+++ libft.h
@@ -11,7 +11,7 @@ int		ft_isdigit(int c);
 int		ft_isalnum(int c);
 int		ft_isascii(int c);
 int		ft_isprint(int c);
-size_t	ft_strlen(const char *str);
+size_t	ft_strlen(const char *s);
 void	*ft_memset(void *ptr, int c, size_t n);
 void	ft_bzero(void *str, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
