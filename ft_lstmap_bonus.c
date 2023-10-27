ebdeb4f5e78e859bd7e38810027140ab0a66ea3e
Allar Klein <aklein@c2r5p5.hive.fi>
2023-10-27 15:28:50 +0300

    lstmap recursive f

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -1,40 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*   ft_lstmap_bonus.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aklein <aklein@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/26 20:34:18 by aklein            #+#    #+#             */
-/*   Updated: 2023/10/26 23:06:06 by aklein           ###   ########.fr       */
+/*   Updated: 2023/10/27 15:27:41 by aklein           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
-{
-	t_list	*new_list;
-	t_list	*add;
-
-	if (!lst)
-		return (NULL);
-	new_list = ft_lstnew(f(lst->content));
-	while (lst)
-	{
-		add = ft_lstnew(f(lst->content));
-		if (!add)
-		{
-			ft_lstclear(&new_list, del);
-			return (NULL);
-		}
-		ft_lstadd_back(&new_list, add);
-		lst = lst->next;
-	}
-	return (new_list);
-}
-
-t_list	*ft_lstmap_test(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new_list;
 
@@ -46,6 +24,6 @@ t_list	*ft_lstmap_test(t_list *lst, void *(*f)(void *), void (*del)(void *))
 		ft_lstclear(&new_list, del);
 		return (NULL);
 	}
-	new_list->next = ft_lstmap_test(lst->next, f, del);
+	new_list->next = ft_lstmap(lst->next, f, del);
 	return (new_list);
 }
